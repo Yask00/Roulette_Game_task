ce54d5c7d00b0d3502a76b3665dc6ce2c2c848f2
Yasen Trayanov <jasstreb@abv.bg>
2025-05-19 18:38:29 +0300

    feat: add styling with mainly flex

--- solution/script.mjs
+++ solution/script.mjs
@@ -15,3 +15,8 @@ chips.forEach(chip => {
 // chips in game should have method any selected or selected and events listeners should happen in game class
 //  Game(chips) // events from cells on clicks / events from actions / bets history per cell
 // events should be Observer pattern
+
+// 1. build table
+// 2. Add event that fires custom event
+// 3. add observer and subscriber pattern
+// 4. finish table logic
